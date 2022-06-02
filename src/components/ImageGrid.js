f58f7465aaa0cbfdfcf17adc9ca17c3f081a2410
Marumbo Sichinga <marumbok@gmail.com>
2022-06-02 17:26:53 +0200

    added bio page, image and text

---
+++ src/components/ImageGrid.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ImageGrid = () => {
+  return (
+    <div class="container grid grid-cols-3 gap-2 mx-auto">
+    <div class="w-full rounded">
+        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="studio" />
+    </div>
+    <div class="w-full rounded">
+        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="studio" />
+    </div>
+    <div class="w-full rounded">
+        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="studio" />
+    </div>
+    <div class="w-full rounded">
+        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="stage" />
+    </div>
+    <div class="w-full rounded">
+        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="stage" />
+    </div>
+    <div class="w-full rounded">
+        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="stage" />
+    </div>
+</div>
+
+  )
+}
+
+export default ImageGrid
